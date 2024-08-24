354014c9603524f07f77b4543984e1f910f09064
Vanja Stojanović <vanja.stojanovic25@gmail.com>
2024-08-24 12:49:39 +0200

    Inital structre

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # http-in-c
-My implementation of a simple HTTP server in C
+
+My implementation of a simple HTTP server in C as defined by [RFC9112](https://datatracker.ietf.org/doc/html/rfc9112).
+
+This is a practice project.
---
+++ src/main.c
@@ -0,0 +1,17 @@
+// Copyright 2024 Vanja Stojanović
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+  // 1. Implement socket listening and accept requests
+  // 2. Parse and read the requests
+  // 3. Respond with appropriate response
+  // It will only support GET and POST methods
+  // and implement all error codes
+  
+  return EXIT_SUCCESS;
+}
